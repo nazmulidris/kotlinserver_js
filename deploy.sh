0ac25003eefe9f56cfa442c43d58a794f11905ee
Nazmul Idris <nazmul@fasterlap.com>
2018-01-07 21:20:49 -0800

    Build script changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # https://devcenter.heroku.com/articles/deploying-gradle-apps-on-heroku
-heroku config:set GRADLE_TASK="build"
+# heroku config:set GRADLE_TASK="build"
+heroku config:set GRADLE_TASK="stage"
 git push heroku master
